bb60f57a6b05e95e0aca79ae753a9b2f39996825
PriyankaDadhich17 <dadhich.priyanka17@gmail.com>
2018-04-10 18:00:21 +0530

    Update lookupInputRenderer.js

--- Lookup/lookupInputRenderer.js
+++ Lookup/lookupInputRenderer.js
@@ -5,7 +5,7 @@
     rerender : function(component, helper){
         this.superRerender();
 		
-		//if value changes, triggers the loading method
+	//if value changes, triggers the loading method
         if(helper.typeaheadOldValue[component.getGlobalId()] !== component.get('v.value')){
             helper.loadValue(component,true);
         }
